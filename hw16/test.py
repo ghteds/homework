7a042943c590349c4e8f7db1c17eff26dc85e87c
BCstag <tstagner@bellsouth.net>
2019-01-03 14:27:10 -0500

    first

---
+++ hw16/test.py
@@ -0,0 +1,14 @@
+numberlist=[1,2,3]
+strlist=['one','two','three']
+
+n,s =zip(*numberlist,*strlist)
+
+
+print(n,s)
+
+# for z in zipped:
+#     print(z)
+
+# x=[i for i in range(10)]
+
+# print (x)
